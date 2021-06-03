41233b8fc5fca471620938e96356e6b10a3f3a6d
Pedro Struck <ppslima@gmail.com>
2021-06-03 14:52:48 -0300

    Extracting method to print list of strings

--- src/com/struck/Application.java
+++ src/com/struck/Application.java
@@ -1,5 +1,7 @@
 package com.struck;
 
+import java.util.List;
+
 import com.struck.enums.FileEnum;
 import com.struck.util.FileHandler;
 import com.struck.util.RandomnessHandler;
@@ -9,9 +11,13 @@ public class Application {
 	public static void main(String[] args) {
 		int min = 1;
 		int max = 10;
-		System.out.println("Random number between: " + min + " and " + max + ": " + RandomnessHandler.getRandomIntInRange(min, max));
+		System.out.println("Random number between " + min + " and " + max + ": " + RandomnessHandler.getRandomIntInRange(min, max));
 		System.out.println("Number of lines in file: " + FileHandler.getNumberOfLinesFromFile(FileEnum.MALE.getName()));
-		for (String line : FileHandler.getLinesFromFile(FileEnum.MALE.getName())) {
+		printListOfStrings(FileHandler.getLinesFromFile(FileEnum.MALE.getName()));
+	}
+	
+	private static void printListOfStrings(List<String> list) {
+		for (String line : list) {
 			System.out.println(line);
 		}
 	}
